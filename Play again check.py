33faca55378abc6732c32ecc5859a84a23b7c3f6
muggerseo <65226654+muggerseo@users.noreply.github.com>
2022-08-05 22:42:31 +0200

    Add files via upload

---
+++ Play again check.py
@@ -0,0 +1,16 @@
+def replay():
+
+        while True:
+            try:
+                #choice not in ('Yes','No'):
+                choice = input("play again? Enter Yes or No ")
+            except ValueError:
+                print("Please print Yes or No exactly")
+                continue
+                
+            if choice == 'Yes':
+                print('New game!')
+                return choice == 'Yes'
+            elif choice == 'No':
+                print('See ya!')
+                break
