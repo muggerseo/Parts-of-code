bd3cab7c2ae46865724e05ba999d6d7c334f9fc8
muggerseo <65226654+muggerseo@users.noreply.github.com>
2022-08-24 21:28:44 +0200

    Need to be fixed

---
+++ PDF password.py
@@ -0,0 +1,13 @@
+from PyPDF2 import PdfFileMerger
+import os
+
+merger = PdfFileMerger()
+for item in os.listdir():
+    if item.endswith('.pdf'):
+        merger.append(item)
+merger.write("Final_pdf.pdf")
+merger = PdfFileMerger()
+with open(originalFile, 'rb') as fin:
+    merger.append(PdfFileReader(fin))
+os.remove(originalFile)
+merger.close
